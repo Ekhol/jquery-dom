17136f0604fe31d5bc37113ca85504d6e24204a4
Nathan <frost.nathan1995@gmail.com>
2022-05-02 16:11:57 -0400

    Tidied up

--- jquery-movies/script.js
+++ jquery-movies/script.js
@@ -2,5 +2,40 @@ let movies = [];
 
 $(function () {
 
-    $()
-})
+    let curId = 0;
+
+    $("#new-movies").on("submit", function (evt) {
+        evt.preventDefault();
+
+        let title = $("#title").val();
+        let rating = $("#rating").val();
+
+        let movieInfo = { title, rating, curId };
+        const HTMLAppend = createHTML(movieInfo);
+
+        curId++;
+        movies.push(movieInfo);
+
+        $("#tableBody").append(HTMLAppend);
+        $("#new-movies").trigger("reset");
+    });
+
+    $("tbody").on("click", ".btn.delete", function (evt) {
+        let toRemove = movies.findIndex(movie => movie.currentId === +$(evt.target).data("deleteId"));
+        movies.splice(toRemove, 1);
+
+        $(evt.target).closest("tr").remove();
+    });
+});
+
+function createHTML(info) {
+    return `<tr>
+        <td>${info.title}</td>
+        <td>${info.rating}</td>
+        <td> 
+            <button class ="btn delete" data-delete-id=${info.curId}>
+                Delete
+            </button>
+        </td>
+    </tr>`;
+}
